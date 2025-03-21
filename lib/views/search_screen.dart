34ffe5d5a92b9363bcc0c3825868073bc4e6b6bf
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-03-21 23:19:34 +0600

    animation added left slider

--- lib/views/search_screen.dart
+++ lib/views/search_screen.dart
@@ -86,12 +86,29 @@ class SearchScreen extends StatelessWidget {
                               fontWeight: FontWeight.w400,
                             ),
                           ),
+                          // onTap: () => Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => DetailsScreen(course: course),
+                          //   ),
+                          // ),
                           onTap: () => Navigator.push(
                             context,
-                            MaterialPageRoute(
-                              builder: (context) => DetailsScreen(course: course),
+                            PageRouteBuilder(
+                              pageBuilder: (context, animation, secondaryAnimation) => DetailsScreen(course: course),
+                              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                                const begin = Offset(1.0, 0.0);
+                                const end = Offset.zero;
+                                const curve = Curves.easeInOut;
+
+                                final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                                final offsetAnimation = animation.drive(tween);
+
+                                return SlideTransition(position: offsetAnimation, child: child);
+                              },
                             ),
                           ),
+
                         ),
                       );
                     },
